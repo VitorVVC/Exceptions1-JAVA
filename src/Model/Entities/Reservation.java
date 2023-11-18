a0eac3d3f3b5fee61ac4ce46694d74817d440a34
Vitor <vitorvvc2005@gmail.com>
2023-11-18 16:46:00 -0300

    Not So Bad Solution

--- src/Model/Entities/Reservation.java
+++ src/Model/Entities/Reservation.java
@@ -21,10 +21,17 @@ public class Reservation {
         return Period.between(checkIn, checkOut);
     }
 
-    public void updateDates(LocalDate checkIn, LocalDate checkOut) {
+    public String updateDates(LocalDate checkIn, LocalDate checkOut) {
+        LocalDate now = LocalDate.now();
+        if (checkIn.isBefore(now) || checkOut.isBefore(now)) {
+            return "Error in reservation: Reservation dates for update must be future dates";
+        }
+        if (!checkOut.isAfter(checkIn)) {
+            return "Error in reservation: Check-out date must be after check-in date";
+        }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
-        System.out.println("New checkin: " + checkIn.format(dtFormatter) + "and new checkout: " + checkOut.format(dtFormatter));
+        return null;
     }
 
     @Override
